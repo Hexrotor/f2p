4543f11223e592f0d76b5a10dffea68f40744550
Hexrotor <tszbbb@qq.com>
2025-08-29 03:34:26 +0800

    Sleep 1s for message deliver

--- internal/server/handlers_control.go
+++ internal/server/handlers_control.go
@@ -25,6 +25,7 @@ func (s *Server) handleControlStream(stream network.Stream) {
 	if !exists {
 		if err := s.handleAuthenticationStream(stream); err != nil {
 			slog.Warn("Authentication failed", "client", remotePeer.String(), "error", err)
+			time.Sleep(1*time.Second)
 			s.host.Network().ClosePeer(remotePeer)
 		}
 		return
